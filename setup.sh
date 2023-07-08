f98765ec58236a2eb7d63d4ccc30c06a65b11abe
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-09 06:38:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
+# // Root Checking
 if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		echo "OpenVZ is not supported"
-		exit 1
-  
-# // Root Checking
-if [ "${EUID}" -ne 0 ]; then
-		echo -e "${EROR} Please Run This Script As Root User !"
 		exit 1
 fi
 clear
