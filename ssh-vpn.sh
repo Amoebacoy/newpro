9e847405461cf8ffdd0ce38b52b066c46d4cf61e
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-09 22:55:22 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -25,7 +25,7 @@ cd
 #END
 
 # Installing Service ws-dropbear
-wget -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSHWS/dropbear-ws"
+wget -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/syapik96/aws/main/websocket-python/ws-dropbear"
 chmod +x /usr/local/bin/ws-dropbear
 
 # Create system Service ws-dropbear
@@ -49,7 +49,7 @@ WantedBy=multi-user.target
 END
 
 # Installing Service ws-stunnel
-wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSHWS/ws-stunnel"
+wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/syapik96/aws/main/websocket-python/ws-stunnel"
 chmod +x /usr/local/bin/ws-stunnel
 
 # Create system Service ws-stunnel
@@ -72,7 +72,7 @@ END
 [Install]
 WantedBy=multi-user.target
 # Installing Service ws-openssh
-wget -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/vinstechmy/SSH-XRAY-Websocket-Multiport/main/SSHWS/openssh-socket"
+wget -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/syapik96/aws/main/websocket-python/ws-openssh"
 chmod +x /usr/local/bin/ws-openssh
 
 # Create system Service ws-openssh
