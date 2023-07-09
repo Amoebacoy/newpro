8b5f2e8aec9b9a7116e607523275cb90ba4e384d
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-09 08:39:48 +0700

    Update ssh-vpn1.sh

--- ssh-vpn1.sh
+++ ssh-vpn1.sh
@@ -142,33 +142,33 @@ if [ "${EUID}" -ne 0 ]; then
 cd
 
 # Getting websocket dropbear
-wget -q -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/wunuit/0/main/ws-dropbear"
-chmod +x /usr/local/bin/ws-dropbear
+#wget -q -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/wunuit/0/main/ws-dropbear"
+#chmod +x /usr/local/bin/ws-dropbear
 
 # Installing Service
-cat > /etc/systemd/system/ws-dropbear.service << END
-[Unit]
-Description=Ssh Websocket By Akhir Zaman
+#cat > /etc/systemd/system/ws-dropbear.service << END
+#[Unit]
+#Description=Ssh Websocket By Akhir Zaman
 #Documentation=https://xnxx.com
-After=network.target nss-lookup.target
+#After=network.target nss-lookup.target
 
 [Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/usr/bin/python2 -O /usr/local/bin/ws-dropbear 8880
-Restart=on-failure
+#Type=simple
+#User=root
+#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+#NoNewPrivileges=true
+#ExecStart=/usr/bin/python2 -O /usr/local/bin/ws-dropbear 8880
+#Restart=on-failure
 
-[Install]
-WantedBy=multi-user.target
-END
+#[Install]
+#WantedBy=multi-user.target
+#END
 
-systemctl daemon-reload >/dev/null 2>&1
-systemctl enable ws-dropbear >/dev/null 2>&1
-systemctl start ws-dropbear >/dev/null 2>&1
-systemctl restart ws-dropbear >/dev/null 2>&1
+#systemctl daemon-reload >/dev/null 2>&1
+#systemctl enable ws-dropbear >/dev/null 2>&1
+#systemctl start ws-dropbear >/dev/null 2>&1
+#systemctl restart ws-dropbear >/dev/null 2>&1
 
 clear 
 
