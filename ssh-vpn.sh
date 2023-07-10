6ca2c41cfd1f4d93c4e041ad5de3189e7c790b83
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-10 09:27:27 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -41,7 +41,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear 2080 
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-dropbear 80 
 Restart=on-failure
 
 [Install]
@@ -65,7 +65,7 @@ User=root
 CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
-ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel 2083
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-stunnel 443
 Restart=on-failure
 
 [Install]
