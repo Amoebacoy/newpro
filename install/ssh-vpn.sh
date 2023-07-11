3e80360f3282eb063b55dbc7edef7181a0949506
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-11 10:23:27 +0700

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -129,26 +129,21 @@ apt -y install sslh
   
 ​#​ Settings SSLH
 cat ​>​ "/etc/default/sslh" ​<<​-EOF12
-​# Default options for sslh initscript 
-​# sourced by /etc/init.d/sslh 
-  
-​# Disabled by default, to force yourself 
-# to read the configuration: 
-# - /usr/share/doc/sslh/README.Debian (quick start) 
-​# - /usr/share/doc/sslh/README, at "Configuration" section 
-​# - sslh(8) via "man sslh" for more configuration details. 
-# Once configuration ready, you *must* set RUN to yes here 
-# and try to start sslh (standalone mode only) 
- 
+   ​# Default options for sslh initscript 
+   ​# sourced by /etc/init.d/sslh 
+​   # Disabled by default, to force yourself 
+   # to read the configuration: 
+   # - /usr/share/doc/sslh/README.Debian (quick start) 
+​   # - /usr/share/doc/sslh/README, at "Configuration" section 
+​   # - sslh(8) via "man sslh" for more configuration details. 
+   # Once configuration ready, you *must* set RUN to yes here 
+   # and try to start sslh (standalone mode only) 
 RUN=yes 
-
-# binary to use: forked (sslh) or single-thread (sslh-select) version 
-​# systemd users: dont forget to modify /lib/systemd/system/sslh.service 
+   # binary to use: forked (sslh) or single-thread (sslh-select) version 
+   ​# systemd users: dont forget to modify /lib/systemd/system/sslh.service 
 ​DAEMON=/usr/sbin/sslh 
-
 DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
-
-EOF12->>
+EOF12
   
 ​#​ Restart Service SSLH 
 service sslh restart 
