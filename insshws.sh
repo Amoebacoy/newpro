f143b0d4e2ca55322af4e2d093f9ced32ae70b2e
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-12 12:57:36 +0700

    Update insshws.sh

--- insshws.sh
+++ insshws.sh
@@ -11,21 +11,21 @@ rm /etc/systemd/system/ws-ovpn.service
 wget -O /etc/systemd/system/ws-ovpn.service "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws/ovpn.service"
 
 # Installing Service ws-dropbear
-wget -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-dropbear"
+wget -O /usr/local/bin/ws-dropbear "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-dropbear.py"
 chmod +x /usr/local/bin/ws-dropbear
 # Create system Service ws-dropbear
 rm /etc/systemd/system/ws-dropbear.service
 wget -O /etc/systemd/system/ws-dropbear.service "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws/ws-dropbear.service"
 
 # Installing Service ws-stunnel
-wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-stunnel"
+wget -O /usr/local/bin/ws-stunnel "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-stunnel.py"
 chmod +x /usr/local/bin/ws-stunnel
 # Create system Service ws-stunnel
 rm /etc/systemd/system/ws-stunnel.service
 wget -O /etc/systemd/system/ws-stunnel.service "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws/ws-stunnel.service"
 
 # Installing Service ws-openssh
-wget -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-openssh"
+wget -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/Amoebacoy/newpro/main/ws-openssh.py"
 chmod +x /usr/local/bin/ws-openssh
 # Create system Service ws-openssh
 rm /etc/systemd/system/ws-openssh.service
