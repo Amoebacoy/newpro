4506ef9098990945d893eab181e9c9461660cfcc
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-13 17:34:10 +0700

    Update vlu4a.sh

--- vlu4a.sh
+++ vlu4a.sh
@@ -210,27 +210,27 @@ echo "=====================-[ Aby & Fabumi Premium ]-===================="
 echo ""
 echo "------------------------------------------------------------"
 echo ""
-echo ""
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
-echo "   - SSH Websocket            : 80 [ON]" | tee -a log-install.txt
+echo "   - SSH Websocket            : 80" | tee -a log-install.txt
 echo "   - SSH SSL Websocket        : 443" | tee -a log-install.txt
-echo "   - Stunnel5                 : 222, 777" | tee -a log-install.txt
+echo "   - Stunnel4                 : 222, 777" | tee -a log-install.txt
 echo "   - Dropbear                 : 109, 143" | tee -a log-install.txt
 echo "   - Badvpn                   : 7100-7900" | tee -a log-install.txt
-echo "   - Nginx                    : 89" | tee -a log-install.txt
-echo "   - Trojan WS                : 443" | tee -a log-install.txt
+echo "   - Nginx                    : 81" | tee -a log-install.txt
 echo "   - Vmess WS TLS             : 443" | tee -a log-install.txt
 echo "   - Vless WS TLS             : 443" | tee -a log-install.txt
+echo "   - Trojan WS TLS            : 443" | tee -a log-install.txt
 echo "   - Shadowsocks WS TLS       : 443" | tee -a log-install.txt
 echo "   - Vmess WS none TLS        : 80" | tee -a log-install.txt
 echo "   - Vless WS none TLS        : 80" | tee -a log-install.txt
+echo "   - Trojan WS none TLS       : 80" | tee -a log-install.txt
 echo "   - Shadowsocks WS none TLS  : 80" | tee -a log-install.txt
 echo "   - Vmess gRPC               : 443" | tee -a log-install.txt
 echo "   - Vless gRPC               : 443" | tee -a log-install.txt
 echo "   - Trojan gRPC              : 443" | tee -a log-install.txt
 echo "   - Shadowsocks gRPC         : 443" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
+echo ""
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
 echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
