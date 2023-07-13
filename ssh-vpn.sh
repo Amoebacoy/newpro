5daebdbdc47a0b34d94edef387f978bf8b9a88a0
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-13 07:43:20 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -135,8 +135,10 @@ cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/Amoebacoy/newpro/main/nginx.conf"
-mkdir -p /home/vps/public_html
 wget -q -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/Amoebacoy/newpro/main/vps.conf"
+# creating page download Openvpn config file
+mkdir -p /home/vps/public_html
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/syapik96/aws/main/lain2/index.html"
 /etc/init.d/nginx restart
 
 # install badvpn
