497cf3827f2e4f46a1aaf1d8a154bc8b75f0c2d2
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-14 11:38:04 +0700

    Update vlu4a.sh

--- vlu4a.sh
+++ vlu4a.sh
@@ -147,18 +147,18 @@ echo -e " \E[41;1;39m          ? Install SSH / WS ?           \E[0m$NC"
 echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
 sleep 1
 wget -q https://raw.githubusercontent.com/Amoebacoy/newpro/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
-#install websocket
-echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
-echo -e " \E[41;1;39m           ? Install websocket ?            \E[0m$NC"
-echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
-sleep 1
-wget -q https://raw.githubusercontent.com/Amoebacoy/newpro/main/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 #install ins-xray
 echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
 echo -e " \E[41;1;39m            ? Install Xray ?             \E[0m$NC"
 echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
 sleep 1 
 wget -q https://raw.githubusercontent.com/Amoebacoy/newpro/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
+#install websocket
+echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
+echo -e " \E[41;1;39m           ? Install websocket ?            \E[0m$NC"
+echo -e "$white\033[0;34m+-----------------------------------------+${NC}"
+sleep 1
+wget -q https://raw.githubusercontent.com/Amoebacoy/newpro/main/insshws.sh && chmod +x insshws.sh && ./insshws.sh
 IP=$(echo $SSH_CLIENT | awk '{print $1}')
 TMPFILE='/tmp/ipinfo-$DATE_EXEC.txt'
 curl http://ipinfo.io/$IP -s -o $TMPFILE
