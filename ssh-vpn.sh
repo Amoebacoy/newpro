2432dd501207e290f7a446f45695aba42b841773
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-15 05:43:17 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -1,6 +1,40 @@
 #!/bin/bash
 #
 # ==================================================
+clear
+#Minacantik
+export LANG='en_US.UTF-8'
+export LANGUAGE='en_US.UTF-8'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+BIRed='\033[1;91m'
+red='\e[1;31m'
+bo='\e[1m'
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+tyblue='\e[1;36m'
+purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
+tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+export EROR="[${RED} ERROR ${NC}]"
+export INFO="[${YELLOW} INFO ${NC}]"
+export OKEY="[${GREEN} OKEY ${NC}]"
+export PENDING="[${YELLOW} PENDING ${NC}]"
+export SEND="[${YELLOW} SEND ${NC}]"
+export RECEIVE="[${YELLOW} RECEIVE ${NC}]"
+export BOLD="\e[1m"
+export WARNING="${RED}\e[5m"
+export UNDERLINE="\e[4m"
+
 cd /root/
 cat > /root/.profile <<-END
 if [ "$BASH" ]; then
@@ -14,6 +48,7 @@ export LANGUAGE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 END
+
 # etc
 cd
 apt dist-upgrade -y
@@ -518,7 +553,7 @@ sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting nginx "
 /etc/init.d/openvpn restart >/dev/null 2>&1
 sleep 0.5
-echo -e "[ ${green}ok${NC} ] Restarting cron "
+echo -e "[ ${green}ok${NC} ] Restarting openvpn "
 /etc/init.d/ssh restart >/dev/null 2>&1
 sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting ssh "
@@ -535,10 +570,10 @@ echo -e "[ ${green}ok${NC} ] Restarting stunnel5 "
 sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting vnstat "
 #/etc/init.d/squid restart >/dev/null 2>&1
-/etc/init.d/cron restart
+/etc/init.d/cron restart >/dev/null 2>&1
 sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting cron "
-/etc/init.d/sslh restart
+/etc/init.d/sslh restart >/dev/null 2>&1
 sleep 0.5
 echo -e "[ ${green}ok${NC} ] Restarting sslh "
 
