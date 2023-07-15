3c71e03547d2f1a35eeda237e9a3f4fecf95c62a
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-15 08:57:14 +0700

    Update ws-stunnel.py

--- ws-stunnel.py
+++ ws-stunnel.py
@@ -2,16 +2,18 @@
 import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
-LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
-
-# Pass
+LISTENING_ADDR = '127.0.0.1'
+if sys.argv[1:]:
+  LISTENING_PORT = sys.argv[1]
+else:
+  LISTENING_PORT = 700  
+#Pass
 PASS = ''
 
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:443'
+DEFAULT_HOST = '127.0.0.1:69'
 RESPONSE = 'HTTP/1.1 101 <b><font color="#E0FFFF">Switching Protocols</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 101 <b><font color="#E0FFFF">AKHIR ZAMAN</font></b>\r\n\r\nContent-Length: 104857600000\r\n\r\n'
 
