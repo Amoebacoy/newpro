cff73140b9536f2d813ec8a77ff958c52ec6e181
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2023-07-15 11:31:12 +0700

    Update auto pionting.sh

--- auto pionting.sh
+++ auto pionting.sh
@@ -2,11 +2,23 @@
 # // String / Request Data
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-apt install update && apt upgrade -y
-apt install socat -y
-apt install jq curl -y
-sub=$(</dev/urandom tr -dc a-z | head -c4)
 clear
+apt install update && apt upgrade -y
+apt install jq curl -y
+#sub=$(</dev/urandom tr -dc a-z | head -c4)
+clear
+echo -e ""
+echo -e "jangan karakter singkat seperti: sg, id, hk,"
+echo -e "kalau bisa 1 kata yang unik dengan dikombinasikan dengan angka"
+echo -e "contoh: resa11"
+echo -e ""
+echo -e "\e[32msubdomain\e[0m.fabumi.my.id"
+read -p "Mau subdomain apa?( 1kata ) : " sub
+if [[ $sub == "" ]]; then
+clear
+echo -e "${EROR} No Input Detected !"
+exit 1
+fi
 DOMAIN=fabumi.my.id
 SUB_DOMAIN=${sub}.fabumi.my.id
 CF_ID=amoeba.coy@gmail.com
@@ -29,14 +41,14 @@ if [[ "${#RECORD}" -le 10 ]]; then
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":0,"proxied":false}' | jq -r .result.id)
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 fi
 
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
      -H "X-Auth-Email: ${CF_ID}" \
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":0,"proxied":false}')
+     --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
      
 echo "Host : $SUB_DOMAIN"
 sleep 1
@@ -45,34 +57,5 @@ yellow "Domain added.."
 sleep 3
 echo -e ""
 echo -e "subdomainmu telah jadi yaitu: $SUB_DOMAIN"
-sleep 1
-echo "$SUB_DOMAIN" > /etc/xray/domain
-nsdomain=ns-${SUB_DOMAIN}
-echo "Updating DNS for ${nsdomain}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${nsdomain}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'ns-${sub}'","content":"'${SUB_DOMAIN}'","ttl":0,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"NS","name":"'ns-${sub}'","content":"'${SUB_DOMAIN}'","ttl":0,"proxied":false}')
-     
-echo "$nsdomain" > /etc/xray/nsdomain
-echo "Domain = $SUB_DOMAIN"
-echo "NS Domain = $nsdomain"
+echo -e "Pakai itu untuk install nanti"
+cd
